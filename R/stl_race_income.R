e108da9f738ad8204204d0976c87cec269677123
Christopher Prener <chris.prener@slu.edu>
2019-04-23 09:17:00 -0500

    add sample data

---
+++ R/stl_race_income.R
@@ -0,0 +1,26 @@
+#' Race and Median Income in St. Louis by Census Tract, 2017
+#'
+#' @description A simple features data set containing the geometry and associated attributes
+#'     for the 2013-2017 American Community Survey estimates for median household income and the
+#'     percentage of white residents in St. Louis.
+#'
+#' @docType data
+#'
+#' @usage data(stl_race_income)
+#'
+#' @format A data frame with 106 rows and 4 variables:
+#' \describe{
+#'   \item{GEOID}{full GEOID string}
+#'   \item{pctWhite}{Percent of white residents per tract}
+#'   \item{medInc}{Median household income of tract}
+#'   \item{geometry}{simple features geometry}
+#'   }
+#'
+#' @source \code{tidycensus} package
+#'
+#' @examples
+#' str(stl_race_income)
+#' head(stl_race_income)
+#' summary(stl_race_income$medInc)
+#'
+"stl_race_income"
---
+++ inst/extdata/create_sample_data.R
@@ -0,0 +1,30 @@
+# create sample data
+
+library(dplyr)
+library(tidycensus)
+library(tigris)
+
+# download median income data
+get_acs(year = 2017, geography = "tract", variables = "B19019_001", state = 29, county = 510) %>%
+  select(GEOID, estimate) %>%
+  rename(medInc = estimate) -> medianInc
+
+# download race data
+get_acs(year = 2017, geography = "tract", table = "B02001", state = 29, county = 510, output = "wide") %>%
+  select(GEOID, B02001_001E, B02001_002E) %>%
+  rename(total = B02001_001E, white = B02001_002E) %>%
+  mutate(pctWhite = white/total*100) %>%
+  select(GEOID, pctWhite) -> race
+
+# download tract geometry
+tracts <- tracts(state = 29, county = 510, class = "sf") %>%
+  select(GEOID)
+
+# combine tract data with geometry
+tracts <- tracts %>%
+  left_join(., race, by = "GEOID") %>%
+  left_join(., medianInc, by = "GEOID")
+
+stl_race_income <- tracts
+
+usethis::use_data(stl_race_income)
