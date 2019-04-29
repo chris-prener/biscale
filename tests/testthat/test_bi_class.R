2c8942060d5459b8075088ed2ee39b81b9c80eec
Christopher Prener <chris.prener@slu.edu>
2019-04-29 10:50:59 -0500

    begin building out bi_class unit testing

--- tests/testthat/test_bi_class.R
+++ tests/testthat/test_bi_class.R
@@ -1,15 +1,22 @@
 context("test bi_class function")
 
+# load suggested dependencies ------------------------------------------------
+
+library(sf)
+
 # load test data ------------------------------------------------
 
 data("stl_race_income", package = "biscale")
 
-# create classes ------------------------------------------------
-
-data <- bi_class(stl_race_income, x = pctWhite, y = medInc)
-
 # test inputs ------------------------------------------------
 
 test_that("correctly specified functions execute without error", {
-  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc), NA)
+  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc, style = "quantile", dim = 2), NA)
+  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc, style = "quantile", dim = 3), NA)
+  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc, style = "equal", dim = 2), NA)
+  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc, style = "equal", dim = 3), NA)
+  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc, style = "fisher", dim = 2), NA)
+  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc, style = "fisher", dim = 3), NA)
+  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc, style = "jenks", dim = 2), NA)
+  expect_error(bi_class(stl_race_income, x = pctWhite, y = medInc, style = "jenks", dim = 3), NA)
 })
