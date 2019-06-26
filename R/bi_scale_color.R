680bfe29ae90eb4293abe64773a3e6c623bc5af1
Christopher Prener <chris.prener@slu.edu>
2019-06-26 12:55:02 -0500

    add bi_scale_color

--- R/bi_scale_color.R
+++ R/bi_scale_color.R
@@ -1,10 +1,10 @@
-#' Apply Bivariate Fill to ggplot Object
+#' Apply Bivariate Color to ggplot Object
 #'
-#' @description Applies the selected palette as the fill aesthetic when \code{\link{geom_sf}}
-#'     is used and the \code{bi_class} variable is given as the \code{fill} in the aesthetic
+#' @description Applies the selected palette as the color aesthetic when \code{\link{geom_sf}}
+#'     is used and the \code{bi_class} variable is given as the \code{color} in the aesthetic
 #'     mapping.
 #'
-#' @usage bi_scale_fill(pal, dim = 3, ...)
+#' @usage bi_scale_color(pal, dim = 3, ...)
 #'
 #' @param pal A palette name; one of \code{"Brown"}, \code{"DkBlue"},
 #'     \code{"DkCyan"}, \code{"DkViolet"}, or \code{"GrPink"}.
@@ -14,31 +14,10 @@
 #'
 #' @return A \code{ggplot} object with the given bivariate palette applied to the data.
 #'
-#' @importFrom ggplot2 scale_fill_manual
-#'
-#' @examples
-#' # load suggested dependencies
-#' library(ggplot2)
-#' library(sf)
-#'
-#' # add breaks, 2x2
-#' data <- bi_class(stl_race_income, x = pctWhite, y = medInc, dim = 2)
-#'
-#' # create map
-#' ggplot() +
-#'   geom_sf(data = data, aes(fill = bi_class), color = "white", size = 0.1, show.legend = FALSE) +
-#'   bi_scale_fill(pal = "GrPink", dim = 2)
-#'
-#' # add breaks, 3x3
-#' data <- bi_class(stl_race_income, x = pctWhite, y = medInc, dim = 3)
-#'
-#' # create map
-#' ggplot() +
-#'   geom_sf(data = data, aes(fill = bi_class), color = "white", size = 0.1, show.legend = FALSE) +
-#'   bi_scale_fill(pal = "GrPink", dim = 3)
+#' @importFrom ggplot2 scale_color_manual
 #'
 #' @export
-bi_scale_fill <- function(pal, dim = 3, ...){
+bi_scale_color <- function(pal, dim = 3, ...){
 
   # check parameters
   if (missing(pal) == TRUE){
@@ -71,6 +50,6 @@ bi_scale_fill <- function(pal, dim = 3, ...){
   }
 
   # apply to ggplot object
-  ggplot2::scale_fill_manual(values = x, ...)
+  ggplot2::scale_color_manual(values = x, ...)
 
 }
